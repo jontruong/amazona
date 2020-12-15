a3b3c175b64a05eeeef110066f50107a35ffbde1
Jonathan Truong <jjtruong90@gmail.com>
2020-12-14 18:44:24 -0800

    Added backend

---
+++ backend/data.js
@@ -0,0 +1,62 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Nike Slim Shirt',
+            image: '/images/p1.jpg',
+            brand: 'Nike',
+            price: 120,
+            countInStock: 10,
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id: '2',
+            name: 'Adidas Slim Shirt',
+            image: '/images/p2.jpg',
+            brand: 'Adidas',
+            price: 50,
+            countInStock: 0,
+            rating: 3,
+            numReviews: 8,
+            description: 'high quality product'
+
+        },
+        {
+            _id: '3',
+            name: 'Nike Slim Pants',
+            image: '/images/p4.jpg',
+            brand: 'Nike',
+            price: 75,
+            countInStock: 20,
+            rating: 4.5,
+            numReviews: 5,
+            description: 'high quality product'
+        },
+        {
+            _id: '4',
+            name: 'Adidas Slim Pants',
+            image: '/images/p5.jpg',
+            brand: 'Adidas',
+            price: 90,
+            countInStock: 12,
+            rating: 4.5,
+            numReviews: 1000,
+            description: 'high quality product'
+        },
+        {
+            _id: '5',
+            name: 'Lacoste Slim Shirt',
+            image: '/images/p3.jpg',
+            brand: 'Lacoste',
+            price: 87,
+            countInStock: 13,
+            rating: 5,
+            numReviews: 90,
+            description: 'high quality product'
+        }
+    ]
+}
+
+export default data;
